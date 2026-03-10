fb71c9df383f02089f5dc3b3eaab4b2568c8e147
chitoglt <chito.eating.cheetos@proton.me>
2026-03-11 00:19:33 +0700

    init

---
+++ website.py
@@ -0,0 +1,15 @@
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv("lfay_data.csv")
+df["Datetime"] = pd.to_datetime(df["Date"] + " " + df["Time"], dayfirst=True)
+fg = px.line(df, x="Datetime", y="Value", 
+             title="lfay_pollution", 
+             labels={"Datetime": "Date", "Value": "PM2.5 (µg/m³)"}, 
+             template="plotly_dark")
+fg.update_layout(
+    font_family="IBM Plex Mono",
+    hovermode="x unified"
+    )
+fg.add_hline(y=15, line_dash="dash", line_color="red", annotation_text="Limite de la WHO")
+fg.show()
