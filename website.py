1846d6abafbb00cae378bf3db3f698562587a693
chitoglt <chito.eating.cheetos@proton.me>
2026-03-11 22:53:50 +0700

    started html

--- website.py
+++ website.py
@@ -7,9 +7,15 @@ fg = px.line(df, x="Datetime", y="Value",
              title="lfay_pollution", 
              labels={"Datetime": "Date", "Value": "PM2.5 (µg/m³)"}, 
              template="plotly_dark")
+
 fg.update_layout(
-    font_family="IBM Plex Mono",
+    font_family="IBM Plex Sans",
     hovermode="x unified"
     )
+
+
 fg.add_hline(y=15, line_dash="dash", line_color="red", annotation_text="Limite de la WHO")
 fg.show()
+
+html_div = fg.to_html(full_html=False, include_plotlyjs='cdn')
+print(html_div)
