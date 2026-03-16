dabc0d32fe7eaabbbfd3bd82114f61e9375f3d12
chitoglt <chito.eating.cheetos@proton.me>
2026-03-16 22:18:29 +0700

    doing changes

--- website.py
+++ website.py
@@ -2,20 +2,42 @@ import plotly.express as px
 import pandas as pd
 
 df = pd.read_csv("lfay_data.csv")
-df["Datetime"] = pd.to_datetime(df["Date"] + " " + df["Time"], dayfirst=True)
-fg = px.line(df, x="Datetime", y="Value", 
-             title="lfay_pollution", 
-             labels={"Datetime": "Date", "Value": "PM2.5 (µg/m³)"}, 
-             template="plotly_dark")
+
+df["datetimeLocal"] = pd.to_datetime(df["datetimeLocal"])
+
+fg = px.line(
+    df,
+    x="datetimeLocal",
+    y="value",
+    color="parameter",
+    title="Air pollution",
+    labels={
+        "datetimeLocal": "Date",
+        "value": "µg/m³",
+        "parameter": "Pollutant"
+    },
+    template="plotly_dark"
+)
 
 fg.update_layout(
     font_family="IBM Plex Sans",
     hovermode="x unified"
-    )
+)
+
+fg.update_xaxes(
+    nticks=10,
+    tickformat="%d %b"
+)
 
 
-fg.add_hline(y=15, line_dash="dash", line_color="red", annotation_text="Limite de la WHO")
+fg.add_hline(
+    y=15,
+    line_dash="dash",
+    line_color="red",
+    annotation_text="WHO limit"
+)
+
 fg.show()
 
-html_div = fg.to_html(full_html=False, include_plotlyjs='cdn')
-print(html_div)
+
+fg.write_html("plot.html", include_plotlyjs="cdn")
